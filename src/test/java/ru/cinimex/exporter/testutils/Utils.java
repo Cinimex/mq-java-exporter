73330d44193389422f0f4b276eaa4b008ab6506d
echerniak <cherniak3@yandex.ru>
2021-03-01 10:16:37 +0300

    Prometheus dependency roll back due to counter rename

--- src/test/java/ru/cinimex/exporter/testutils/Utils.java
+++ src/test/java/ru/cinimex/exporter/testutils/Utils.java
@@ -3,7 +3,7 @@ package ru.cinimex.exporter.testutils;
 import io.prometheus.client.CollectorRegistry;
 
 public class Utils {
-    public static double getPrometheusMetricValue(CollectorRegistry registry, String metricName,
+    public static Double getPrometheusMetricValue(CollectorRegistry registry, String metricName,
                                                   String[] labelNames, String[] labelValues) {
         return registry.getSampleValue(metricName, labelNames, labelValues);
     }
